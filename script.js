59474c7f6601b6200eb4a7bfac8bcd2d213d3ea4
ErbLoic <lerbuer@gmail.com>
2025-02-25 12:59:24 +0100

    re

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Merci pour votre message !');
-    // Ajoutez ici le code pour envoyer le formulaire via une API ou autre
+
+    // Récupérez les valeurs du formulaire
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    // Envoyez l'email via EmailJS
+    emailjs.send("service_uoi2jox", "template_9t6ro7q", {
+        from_name: name,
+        from_email: email,
+        message: message
+    }).then(function(response) {
+        alert('Votre message a été envoyé avec succès !');
+    }, function(error) {
+        alert('Il y a eu une erreur lors de l\'envoi de votre message. Veuillez réessayer.');
+    });
 });
