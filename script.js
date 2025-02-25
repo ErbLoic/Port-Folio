0c2366ee013e42fcc14adbbca39c9dec373638bf
ErbLoic <lerbuer@gmail.com>
2025-02-25 12:37:47 +0100

    Site de base

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Merci pour votre message !');
+    // Ajoutez ici le code pour envoyer le formulaire via une API ou autre
+});
